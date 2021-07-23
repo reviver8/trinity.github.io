8a5b7101afa533bfc4fe75838a18f25b01e31061
reviver8 <85811134+reviver8@users.noreply.github.com>
2021-07-22 20:52:47 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+/*slideshow tings*/
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+/*button tings*/
+var inputBtn = document.getElementById("inputBtn");
+inputBtn.addEventListener("click", displayMotivation);
+
+var displayMessage = document.getElementById("message");
+
+function displayMotivation(){
+  var inputText = document.getElementById("inputText").value;
+  displayMessage.innerHTML = inputText + ", " + "I'm proud of the progress I'm making, no matter how small!";
+}
+
+
